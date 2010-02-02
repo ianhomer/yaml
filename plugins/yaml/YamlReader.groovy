ae8127ca6aee2ec3ed188b2d3fca065eef6793b0
Ian Homer <ian.homer@bemoko.com>
2010-02-02 21:26:05 +0000

    Update YamlReader

--- plugins/yaml/YamlReader.groovy
+++ plugins/yaml/YamlReader.groovy
@@ -13,25 +13,25 @@
  * 
  * Copyright 2010 bemoko 
  */
-import com.bemoko.live.platform.mwc.plugins.AbstractPlugin
-import org.ho.yaml.Yaml
 
-class YamlReader extends AbstractPlugin {
-  def data, filepath, encoding
-  def DEFAULT_ENCODING="UTF-8"
+class YamlReader extends com.bemoko.live.platform.mwc.plugins.AbstractPlugin {
+  def data
+  private static DEFAULT_ENCODING="UTF-8"
 
   /**
    * Load the yaml data
    */
   void initialise(Map p) {
-    filepath = p.get("filepath")
-    encoding = p.get("encoding") ?: DEFAULT_ENCODING
-    String resource = this.findResource(filepath)
-    if (resource == null) throw new Exception("Can't find " + filepath)
+    def resource = findResource(p.filepath)
+    if (!resource) throw new Exception("Can't find ${p.filepath}")
     def inputStream
     try {
       inputStream = new FileInputStream(new File(resource))
-      data = Yaml.load(new BufferedReader(new InputStreamReader(inputStream,encoding)))
+      data = org.ho.yaml.Yaml.load(
+        new BufferedReader(
+          new InputStreamReader(inputStream, p.encoding ?: DEFAULT_ENCODING)
+        )
+      )
     } finally {
       org.apache.commons.io.IOUtils.closeQuietly(inputStream)
     }
